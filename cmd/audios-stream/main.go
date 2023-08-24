ff28a99c4f51aca95c82cbf71f5b5d44d4f57ed5
Eric Oliveira <oiericoi@hotmail.com>
2023-08-24 17:13:12 -0300

    refactor: modify file name

---
+++ cmd/audios-stream/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ericoliveiras/audios-stream-go/handlers"
+)
+
+const port = 8080
+
+func main() {
+	http.HandleFunc("/musics", handlers.AudioStreamHandler)
+	fmt.Printf("Server listening on port %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
